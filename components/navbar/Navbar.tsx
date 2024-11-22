13782bc17ac8f7dee18ae4fa4961c2b8ea67695d
Miguel CAballero <miguelhitogarciaalturo@gmail.com>
2024-11-22 15:04:42 -0500

    se ajusta proyecto

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { HomeIcon } from "@primer/octicons-react";
 import { ActiveLink } from "../active-link/ActiveLink";
 
 const navItems = [
@@ -12,7 +11,6 @@ export const NavBar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <Link href={"/"} className="flex items-center">
-        <HomeIcon className="mr-2" />
         <span>Home</span>
       </Link>
 
